09801d4b8e89e361b5cb0cea00e815318b3eb244
Sarah Le Cam <slecam@capsulecares.com>
2021-12-02 00:33:24 -0500

    Day 2

--- src/day_02/solution.kt
+++ src/day_02/solution.kt
@@ -3,20 +3,59 @@ package day_02
 import readInput
 import readTestInput
 
+class ShipPosition {
+    var depth = 0
+    var horizontal = 0
+    private var aim = 0
+
+    fun getMoveScore() = depth * horizontal
+
+    fun moveBasic(direction: String, amount: Int) {
+        when (direction) {
+            "up" -> depth -= amount
+            "down" -> depth += amount
+            "forward" -> horizontal += amount
+        }
+    }
+
+    fun moveWAim(direction: String, amount: Int) {
+        when (direction) {
+            "up" -> aim -= amount
+            "down" -> aim += amount
+            "forward" -> {
+                horizontal += amount
+                depth += aim * amount
+            }
+        }
+    }
+}
+
 fun part1(input: List<String>): Int {
-    return input.size
+    val shipPosition = ShipPosition()
+
+    input
+        .map { it.split(" ") }
+        .forEach { shipPosition.moveBasic(it.first(), it.last().toInt()) }
+
+    return shipPosition.getMoveScore()
 }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    val shipPosition = ShipPosition()
+
+    input
+        .map { it.split(" ") }
+        .forEach { shipPosition.moveWAim(it.first(), it.last().toInt()) }
+
+    return shipPosition.getMoveScore()
 }
 
 fun main() {
-    val input = readInput(1)
-    val testInput = readTestInput(1)
+    val input = readInput(2)
+    val testInput = readTestInput(2)
 
-//    check(part1(testInput) == 7)
-//    check(part2(testInput) == 7)
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     println(part1(input))
     println(part2(input))
